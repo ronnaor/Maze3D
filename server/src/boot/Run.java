a9071d5970655b67d24fe77be4eb0fffe9f6a4d6
Ron <ron.naor@gmail.com>
2015-10-21 17:43:32 +0300

    server boot Run

--- server/src/boot/Run.java
+++ server/src/boot/Run.java
@@ -1,9 +1,23 @@
 package boot;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+import model.MyModel;
+import presenter.Presenter;
+import view.MyView;
+
+
 public class Run {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		MyModel model=new MyModel();
+		MyView view=new MyView(new BufferedReader(new InputStreamReader(System.in)),new PrintWriter(System.out));
+		Presenter presenter = new Presenter(model, view);
+		model.addObserver(presenter);
+		view.addObserver(presenter);
+		view.start();
 
 	}
 
